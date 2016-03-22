f25e134d627d1d483349933fe2484554c394dda0
thanksdanny <hj-07@163.com>
2016-03-22 17:45:53 +0800

    砖块掉落

--- DropIt/ViewController.m
+++ DropIt/ViewController.m
@@ -9,10 +9,60 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UIView *gameView;
+@property (nonatomic, strong) UIDynamicAnimator *animator;
+@property (nonatomic, strong) UIGravityBehavior *gravity;
 
 @end
 
 @implementation ViewController
+
+static const CGSize DROP_SIZE = {40, 40};
+- (UIDynamicAnimator *)animator {
+    if (!_animator) {
+        _animator = [[UIDynamicAnimator alloc] initWithReferenceView:self.gameView];
+    }
+    return _animator;
+}
+
+- (UIGravityBehavior *)gravity {
+    if (!_gravity) {
+        _gravity = [[UIGravityBehavior alloc] init];
+        _gravity.magnitude = 0.9;
+        [self.animator addBehavior:_gravity];
+    }
+    return _gravity;
+}
+
+- (IBAction)tap:(UITapGestureRecognizer *)sender {
+    [self drop];
+}
+
+- (void)drop {
+    CGRect frame;
+    frame.origin= CGPointZero;
+    frame.size = DROP_SIZE;
+    
+    int x = (arc4random() % (int)self.gameView.bounds.size.width) / DROP_SIZE.width;
+    frame.origin.x = x * DROP_SIZE.width;
+    
+    UIView *dropView = [[UIView alloc] initWithFrame:frame];
+    dropView.backgroundColor = [self randomColor];
+    [self.gameView addSubview:dropView];
+    
+    [self.gravity addItem:dropView]; 
+}
+
+- (UIColor *)randomColor {
+    switch (arc4random() % 5) {
+        case 0: return [UIColor greenColor];
+        case 1: return [UIColor blueColor];
+        case 2: return [UIColor orangeColor];
+        case 3: return [UIColor redColor];
+        case 4: return [UIColor purpleColor];
+    }
+    return [UIColor blackColor];
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
